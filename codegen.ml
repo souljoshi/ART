d10e70235085a3f204b6cc77c9bfdfb7a0eb3802
Gedion Yonas Metaferia <gedionyonas234@gmail.com>
2016-11-08 04:03:51 -0500

    Codegen completed and hello.art test

--- codegen.ml
+++ codegen.ml
@@ -28,10 +28,10 @@ let translate prog =
     (* This two helper functions suggest that that fdecls needs to be modified *)
     (* Takes a function and return the list of declarations *)
     let locals_of_fdecl fdecl = match fdecl.A.body with
-        Block(decls, stmts) -> decls
+        A.Block(decls, stmts) -> List.map (fun (a,b,_) -> (a,b)) decls
       | _ -> raise (Failure "Illegal function block") (* guaranteed to never happen *)
     and body_of_fdecl fdecl = match fdecl.A.body with
-        Block(decls, stmts) -> stmts
+        A.Block(decls, stmts) -> stmts
       | _ -> raise (Failure "Illegal function block") (* guaranteed to never happen *)
     in
 
@@ -81,18 +81,18 @@ let translate prog =
            declared variables.  Allocate each on the stack, initialize their
            value, if appropriate, and remember their values in the "locals" map *)
         let local_vars =
-          (* Arguments: map (type, name) param  (llvm  of params)*)
-          let add_formal m (t, n) p = L.set_value_name n p;
+          (* Arguments: map (type, name,_) param  (llvm  of params)*)
+          let add_formal m (t, n,_) p = L.set_value_name n p;
           (* name appended with nums as necessary: eg  a1,a2 *)
           (* Look at microc lecture: pg 39 *)
             let local = L.build_alloca (ltype_of_typ t) n builder in (* allocate stack for params *)
             ignore (L.build_store p local builder); (* Store the param in the allocated place *)
             StringMap.add n local m in (* We add the stack version *)
           let add_local m (t,n) =
-            let local_var = L.build_alloca (ltype_of_type t) n builder (* allocate space for local *)
+            let local_var = L.build_alloca (ltype_of_typ t) n builder (* allocate space for local *)
             in StringMap.add n local_var m in
 
-          let formals = List.fold_left2 add_formal StringMap.empty fdecl.A.formals
+          let formals = List.fold_left2 add_formal StringMap.empty fdecl.A.params
             (Array.to_list (L.params the_function)) in
           List.fold_left add_local formals (locals_of_fdecl fdecl) in
 
@@ -106,13 +106,13 @@ let translate prog =
 
         let rec expr builder = function (* Takes args builder and Ast.expr *)
             A.IntLit i -> L.const_int i32_t i
-          | A.CharLit c -> L.const_int i8_t c (* 1 byte characters *)
+          | A.CharLit c -> L.const_int i8_t (int_of_char c) (* 1 byte characters *)
           | A.Noexpr -> L.const_int i32_t 0  (* No expression is 0 *)
-          | A.ID s -> L.build_load (lookup s) s builder (* Load the variable into register and return register *)
+          | A.Id s -> L.build_load (lookup s) s builder (* Load the variable into register and return register *)
           | A.Binop (e1, op, e2) ->
               let e1' = expr builder e1
               and e2' = expr builder e2 in
-              match op with
+              (match op with
                   A.Add     -> L.build_add  e1' e2' "tmp" builder
                 | A.Sub     -> L.build_sub  e1' e2' "tmp" builder
                 | A.Mult    -> L.build_mul  e1' e2' "tmp" builder
@@ -129,27 +129,95 @@ let translate prog =
                 | A.Geq     -> L.build_icmp L.Icmp.Sge e1' e2' "tmp" builder
 
                 (* Need to move assignment away from bin *)
-                | A.Asn -> ignore (L.build_store e2 (lookup
+                | A.Asn -> ignore (L.build_store e2' (lookup
                         (match e1 with (* Only works with ID *)
-                          A.ID s -> s
+                          A.Id s -> s
                         | _   -> raise (Failure "Assignment only allowed to identifier") )
-                    ) builder ); e1
-                | _  -> raise (Failure "Unsupported binary op")(* Ignore other binary ops *)
+                    ) builder ); e2'
+                | _  -> raise (Failure "Unsupported binary op") )(* Ignore other binary ops *)
           | A.Unop(op, e) ->
               let e' = expr builder e in
               (match op with
                 A.Neg     -> L.build_neg
-              | A.Not     -> L.build_not) e' "tmp" builder
+              | A.Not     -> L.build_not
               | _  -> raise (Failure "Unsupported unary op")(* Ignore other unary ops *)
+                ) e' "tmp" builder
 
-          | A.Call (ID "printi", [e]) -> L.build_call print_func [|int_format_str ; (expr builder e) |] "printf" builder
-          | A.Call (ID "printc", [e]) -> L.build_call print_func [|char_format_str ; (expr builder e) |] "printf" builder
-          | A.Call (ID f, act) ->
+
+            (* This ok only for few built_in functions *)
+          | A.Call (A.Id "printi", [e]) -> L.build_call printf_func [|int_format_str ; (expr builder e) |] "printf" builder
+          | A.Call (A.Id "printc", [e]) -> L.build_call printf_func [|char_format_str ; (expr builder e) |] "printf" builder
+          | A.Call (A.Id f, act) ->
              let (fdef, fdecl) = StringMap.find f function_decls in
-             let actuals = List.rev (List.map (exr builder) (List.rev act)) in
-             let result = (match fdecl.A.typ with A.Void -> ""
+             (* This makes right to left evaluation order. What order should we use? *)
+             let actuals = List.rev (List.map (expr builder) (List.rev act)) in
+             let result = (match fdecl.A.rettyp with A.Void -> "" (* don't name result for void llvm issue*)
                                                 | _ -> f^"_result") in
              L.build_call fdef (Array.of_list actuals) result builder
           |  _  -> raise (Failure "Unsupported expression")(* Ignore other expressions *)
         in
+        (* Invoke "f builder" if the current block doesn't already
+           have a terminal (e.g., a branch). *)
+        let add_terminal builder f =
+            match L.block_terminator (L.insertion_block builder) with
+                Some _ -> ()
+              | None -> ignore (f builder) in
+
+        (* Build the code for the given statement; return the builder for
+         the statement's successor *)
+        let rec stmt builder = function
+              A.Block (vl, sl) -> List.fold_left stmt builder sl (* Ignore the decls for now *)
+            | A.Expr e -> ignore (expr builder e); builder  (* Simply evaluate expression *)
+
+            | A.Return e -> ignore (match fdecl.A.rettyp with  (* Different cases for void and non-void *)
+                A.Void -> L.build_ret_void builder
+                | _ -> L.build_ret (expr builder e) builder); builder
+            | A.If (predicate, then_stmt, else_stmt) ->
+                let bool_val = expr builder predicate in
+                let merge_bb = L.append_block context "merge" the_function in (* Merge block *)
+                let then_bb = L.append_block context "then" the_function in
+                (* Get the builder for the then block, emit then_stmt and then add branch statement to
+                    merge block *)
+                add_terminal (stmt (L.builder_at_end context then_bb) then_stmt)
+                    (L.build_br merge_bb);
+
+                let else_bb = L.append_block context "else" the_function in
+                add_terminal (stmt (L.builder_at_end context else_bb) else_stmt)
+                    (L.build_br merge_bb);
+                (* add_terminal used to avoid insert two terminals to basic blocks *)
+
+                (* builder is in block that contains if stmt *)
+                ignore (L.build_cond_br bool_val then_bb else_bb builder);
+                L.builder_at_end context merge_bb (* Return builder at end of merge block *)
+
+            | A.While (predicate, body) ->
+                let pred_bb = L.append_block context "while" the_function in
+                ignore (L.build_br pred_bb builder); (* builder is in block that contains while stm *)
+
+                let body_bb = L.append_block context "while_body" the_function in
+                add_terminal (stmt (L.builder_at_end context body_bb) body)
+                (L.build_br pred_bb);
+
+                let pred_builder = L.builder_at_end context pred_bb in
+                let bool_val = expr pred_builder predicate in
+
+                let merge_bb = L.append_block context "merge" the_function in
+                ignore (L.build_cond_br bool_val body_bb merge_bb pred_builder);
+                L.builder_at_end context merge_bb
+
+            (*  make equivalent while *)
+            | A.For (e1, e2, e3, body) -> stmt builder
+                ( A.Block ( [], [  A.Expr e1; A.While (e2, A.Block ([], [body; A.Expr e3]) ) ] ) )
+            | _  -> raise (Failure "Unsupported statement type")(* Ignore other statement type *)
+        in
+        (* Build the code for each statement in the function *)
+        let builder = stmt builder fdecl.A.body in
+
+        (* Add a return if the last block falls off the end *)
+        add_terminal builder (match fdecl.A.rettyp with
+            A.Void -> L.build_ret_void
+          | t -> L.build_ret (L.const_int (ltype_of_typ t) 0))
+    in
+
+    List.iter build_function_body functions;
     the_module
