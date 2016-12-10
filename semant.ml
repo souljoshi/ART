c850e9204ca59a44b68c201733314c785ebc2e4d
metalgearay <baj2125@columbia.edu>
2016-12-10 17:16:37 -0500

    Fixed erros regaring dup struct functions

--- semant.ml
+++ semant.ml
@@ -13,13 +13,42 @@ let report_dup  exceptf list =
         in helper(List.sort compare list)
 
 
+let struct_build prog =
+    let globals = prog.v
+    and functions = prog.f 
+
+    in  
+    (* A map of all struct/shape types *)
+    let structs = List.fold_left ( fun m st -> StringMap.add st.sname st m)
+                StringMap.empty prog.s in
+    let (structs,funcs) = (* Refers to structs and non-member functions *)
+        (* Puts methods and constructors with appropriate struct and returns tuple
+            (map, bool) *)
+        let filter_func m f =
+          match f.typ with
+            Func -> (m, true) (* true means keep function *)
+          | Constructor -> let s = try StringMap.find f.owner m
+                    with Not_found -> raise (Failure ("constructor of undefined struct: " ^ f.owner^"::"^f.fname))
+                in (StringMap.add s.sname
+                    {ss = s.ss;sname = s.sname; decls = s.decls; ctor = f; methods = s.methods} m , false)
+          | Method -> let s = try StringMap.find f.owner m
+                    with Not_found -> raise (Failure ("method of undefined struct: " ^ f.owner^"::"^f.fname))
+                in (StringMap.add s.sname
+                    {ss = s.ss;sname = s.sname; decls = s.decls; ctor = s.ctor; methods = f::s.methods} m , false)
+        in
+        List.fold_left ( fun (m,l) f -> let (m, cond) = filter_func m f in
+        if cond then (m, f::l) else (m, l) ) (structs, []) functions
+    in
+    { s = List.map (fun st -> StringMap.find st.sname structs) prog.s;
+      f = List.rev funcs ; v = globals }
+
 let check prog =
     (* Get the global variables and functions *)
     let globals = prog.v
     and functions = prog.f in
 
     
-    (*report_dup(fun n-> "Duplicate Function Name " ^n)(List.map (fun fd -> fd.fname)functions);*)
+    report_dup(fun n-> "Duplicate Function Name " ^n)(List.map (fun fd -> fd.fname)functions);
 
     report_dup(fun n-> "Duplicate Global Name " ^n)(List.map (fun (_,a,_) ->  a)globals);
 
@@ -47,34 +76,7 @@ in
     report_dup(fun n-> "Duplicate Parameter Name " ^n ^"in " ^ func.fname)(List.map (fun (_,a,_) ->  a)func.params);
     report_dup(fun n-> "Duplicate local Name " ^n ^ " in " ^ func.fname)(List.map (fun (_,a,_) ->  a)func.locals);
 in
-  List.iter function_check functions;
-
-
-
-
-    (* A map of all struct/shape types *)
-    let structs = List.fold_left ( fun m st -> StringMap.add st.sname st m)
-                StringMap.empty prog.s in
-    let (structs,funcs) = (* Refers to structs and non-member functions *)
-        (* Puts methods and constructors with appropriate struct and returns tuple
-            (map, bool) *)
-        let filter_func m f =
-          match f.typ with
-            Func -> (m, true) (* true means keep function *)
-          | Constructor -> let s = try StringMap.find f.owner m
-                    with Not_found -> raise (Failure ("constructor of undefined struct: " ^ f.owner^"::"^f.fname))
-                in (StringMap.add s.sname
-                    {ss = s.ss;sname = s.sname; decls = s.decls; ctor = f; methods = s.methods} m , false)
-          | Method -> let s = try StringMap.find f.owner m
-                    with Not_found -> raise (Failure ("method of undefined struct: " ^ f.owner^"::"^f.fname))
-                in (StringMap.add s.sname
-                    {ss = s.ss;sname = s.sname; decls = s.decls; ctor = s.ctor; methods = f::s.methods} m , false)
-        in
-        List.fold_left ( fun (m,l) f -> let (m, cond) = filter_func m f in
-        if cond then (m, f::l) else (m, l) ) (structs, []) functions
-    in
-    { s = List.map (fun st -> StringMap.find st.sname structs) prog.s;
-      f = List.rev funcs ; v = globals }
+    List.iter function_check functions;
 
 
   
